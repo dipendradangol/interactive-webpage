bbf0bdad4b599b6f40ab8ad1cf8731e939e7fcc0
dipendradangol <dipendra.dangol11@gmail.com>
2019-10-01 23:44:09 +0300

    Updates

---
+++ precache-manifest.b2317d7807dc5ddc30048fc676c7550e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c7994e77006d49614e81cb4796e197",
+    "url": "/interactive-webpage/index.html"
+  },
+  {
+    "revision": "e36b3241fd505bdb2ccf",
+    "url": "/interactive-webpage/static/css/main.af79bdff.chunk.css"
+  },
+  {
+    "revision": "e0ba5f05771b8d7c3e66",
+    "url": "/interactive-webpage/static/js/2.7cb610b5.chunk.js"
+  },
+  {
+    "revision": "e36b3241fd505bdb2ccf",
+    "url": "/interactive-webpage/static/js/main.46f85789.chunk.js"
+  },
+  {
+    "revision": "d7b151ce7acd5c77e80d",
+    "url": "/interactive-webpage/static/js/runtime~main.bae76512.js"
+  },
+  {
+    "revision": "e22127ec8c48ab058c7bfd04ddabbef8",
+    "url": "/interactive-webpage/static/media/background.e22127ec.jpg"
+  },
+  {
+    "revision": "f58239f12cb6d185793cf217e6f9c3c7",
+    "url": "/interactive-webpage/static/media/car.f58239f1.jpg"
+  },
+  {
+    "revision": "47e1f3180aa90f3a8325848c923dc73c",
+    "url": "/interactive-webpage/static/media/logo.47e1f318.png"
+  }
+]);
